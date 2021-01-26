d3e82f1f497c4cc511d48d3e04b4c91fd207c559
JaziraU <78000500+JaziraU@users.noreply.github.com>
2021-01-25 23:04:15 -0600

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,194 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Employee {
+
+
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String employeeId;
+    private String startDate;
+    private String title;
+    private String department;
+
+
+    public Employee(String firstName, String lastName, String phoneNumber, String email, String startDate, String title, String department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.employeeId = generateEmployeeId();
+        this.startDate = startDate;
+        this.title = title;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", employeeId='" + employeeId + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", title='" + title + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+
+    /*
+    1- Create instance variables for employee firstName, lastName, phoneNumber, email, employeeId, startDate,
+    title, department)
+    2- Create getter and setter for instance variables
+    3- Create the constructor to set the value to the instance variables
+    4- Create one method to create the employeeId length of 20. This employee id must have letters as well.
+
+       */
+
+    public  Employee createEmployee(String firstName, String lastName, String phoneNumber,String email, String startDate, String title, String department){
+       // Employee employee =new Employee(firstName,lastName,email,employeeId,startDate,title,department);
+
+    /*
+    this method need to return Employee according to the parameter
+    call the constructor inside this method.
+     */
+      //  String firstName, String lastName, String phoneNumber, String email, String startDate, String title, String department
+        return new Employee( firstName,lastName,phoneNumber,email,startDate,title,department);
+    }
+
+    public static String generateEmployeeId(){
+        ArrayList<Character> letters= new ArrayList<>();
+        for (char i = 97; i <123 ; i++) {
+            char character=i;
+            letters.add(character);
+        }
+        for (char i = 48; i < 58; i++) {
+            letters.add(i);
+        }
+
+        String id="";
+        for (int i = 0; i < 8; i++) {
+            id+= letters.get(randomNumber(letters.size()));
+
+        }
+        id+="-";
+        for (int i = 0; i < 4; i++) {
+            id+= letters.get(randomNumber(letters.size()));
+
+        }
+        id+="-";
+        for (int i = 0; i < 4; i++) {
+            id+= letters.get(randomNumber(letters.size()));
+
+        }
+        id+="-";
+        for (int i = 0; i < 4; i++) {
+            id+= letters.get(randomNumber(letters.size()));
+
+        }
+        return id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        /*
+        this method needs to return the employeeId Length of 20 which includes numbers as well.
+        EmployeeId structure must be length of 20 UUID
+        Example: 61c49c2e-7dcd-11ea-bc55
+         */
+    }
+
+
+    public static int randomNumber(int boundary){
+        Random random=new Random();
+        int randomInt= random.nextInt(boundary);
+        return randomInt;
+    }
+
+
+
+}
---
+++ HRDepartment.java
@@ -0,0 +1,120 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HRDepartment {
+
+
+    private String location;
+    private String companyName;
+    private List<Employee> allCards;
+
+
+    public HRDepartment(String location, String companyName){
+        this.location=location;
+        this.companyName=companyName;
+        this.allCards=new ArrayList<Employee>();
+    }
+
+    public List<Employee> getAllCards() {
+        return allCards;
+    }
+
+    public void setAllCards(ArrayList<Employee> allCards) {
+        this.allCards = allCards;
+    }
+
+    public boolean addEmployee(Employee card){
+
+        if(employeeChecker(card)==-1){
+            allCards.add(card);
+            return true;
+        }
+        else {
+            System.out.println("This employee is already in the list");
+            return false;
+        }
+
+        /*
+        This method will take the parameter as a Employee object and
+        if the Employee is not added to the list before it will add to the arraylist.
+        To be able to understand employee is registered or not you need to check employeeId.
+        Because employee id is unique value.
+        Also, This company can hold up to 20 employee.
+        NOTE: The company has 6 department and in each department can have max 5 employee.
+
+         */
+    }
+
+    public void printEmailAddress(String departmentName){
+
+        for (int i = 0; i < allCards.size(); i++) {
+            if(allCards.get(i).getDepartment().equalsIgnoreCase(departmentName)){
+                System.out.println("Email address : " + allCards.get(i).getEmail());
+            }
+
+        }
+
+        /*
+        this method is taking one parameter department name and it will print all the email address.
+         which is available in this list related with department id
+         */
+    }
+
+    public boolean updateEmailAddress(String employeeId, String newEmailAddress){
+
+        int position=-1;
+        for (int i = 0; i < allCards.size(); i++) {
+            if(allCards.get(i).getDepartment().equalsIgnoreCase(employeeId)){
+                position=i;
+            }
+            }
+
+        if(position==-1){
+            System.out.println("There is no employee with that ID number");
+            return false;
+
+        }
+        else {
+            allCards.get(position).setEmail(newEmailAddress);
+            System.out.println("Email updated successfully");
+            return true;
+        }
+
+
+        /*
+        This method is taking parameter as employeeId and email address.
+        According to the employeeId, you will update the old email address with new email.
+        if it is updated this method will return true else return false
+         */
+    }
+
+
+    public int employeeChecker(Employee employee){
+        return allCards.indexOf(employee);
+    }
+
+
+
+
+
+
+
+
+
+    /*
+    REST OF THE METHODS
+        1- Create the method will take parameter as employeeId and new phone number, it will update old phone number with new phone number.
+        2- Create the method will take parameter as emailAddress and newTitle, it will update old title with new title.
+        3- Create the method will take one parameter as a employeeId and it will print all other information of employee like
+        name, email, phone, title, department etc.
+        4- Create the method will take one parameter as a employeeId and it will remove the employee from the list.
+        5- Create the method will take no parameter and it will print all the department names with employee numbers
+        The Company has 6 different department Finance, Marketing, IT, HR, Purchasing and Sales department.
+        For example:
+        Finance department - 10 employee
+        Marketing department - 10 employee
+        IT department - 10 employee
+        ...
+        .
+        */
+}
